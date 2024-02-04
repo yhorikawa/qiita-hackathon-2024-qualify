77a7f613a68b49b7e5c8c570810ffa306bf3dc97
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-02-04 07:51:25 +0000

    hono client対応

--- backend/src/api/v1/customers.ts
+++ backend/src/api/v1/customers.ts
@@ -2,11 +2,9 @@ import { Hono } from "hono";
 import * as db from "../../gen/sqlc/querier";
 import { Bindings } from "./index";
 
-const customerApi = new Hono<{ Bindings: Bindings }>();
-
-customerApi.get("/", async (c) => {
+const app = new Hono<{ Bindings: Bindings }>().get("/", async (c) => {
   const { results } = await db.customers(c.env.DB);
   return c.json(results);
 });
 
-export default customerApi;
+export default app;
--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -6,12 +6,10 @@ export type Bindings = {
   DB: D1Database;
 };
 
-const api = new Hono<{ Bindings: Bindings }>();
-
-api.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-api.route("customers", customerApi);
+const api = new Hono<{ Bindings: Bindings }>()
+  .get("/", (c) => {
+    return c.text("Hello Hono!");
+  })
+  .route("/customers", customerApi);
 
 export default api;
