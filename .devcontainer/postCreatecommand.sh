5b71222b83a1f04dc0977584ad5d9d6c3d1981f1
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-02-04 13:15:04 +0000

    add generate scripts

--- .devcontainer/postCreatecommand.sh
+++ .devcontainer/postCreatecommand.sh
@@ -9,13 +9,7 @@ pnpm install
 
 ## Create wrangler.toml
 if [ ! -f "./backend/wrangler.toml" ]; then
-  cat << EOF > ./backend/wrangler.toml
-[[d1_databases]]
-binding = "DB" # i.e. available in your Worker on env.DB
-database_name = "qiita-hackathon-2024-qualify"
-database_id = "00000000-0000-0000-0000-000000000000" # Note: 0000... UUID is dummy for local development.
-preview_database_id = "local-test-db"
-EOF
+  pnpm -C backend run generate:wrangler
 fi
 
 ## migration
