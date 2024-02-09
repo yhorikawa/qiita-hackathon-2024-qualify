96d79e9ffe7081d3eedff600790a8670423bde4c
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-02-09 23:40:08 +0000

    backend deploy

--- .devcontainer/postCreatecommand.sh
+++ .devcontainer/postCreatecommand.sh
@@ -6,12 +6,5 @@ pnpm config set store-dir /home/node/.local/share/pnpm/store
 ## Install dependencies
 pnpm install
 
-# /backend
-
-## Create wrangler.toml
-if [ ! -f "./backend/wrangler.toml" ]; then
-  pnpm -C backend run generate:wrangler
-fi
-
 ## migration
 pnpm -C backend run migrate:local
