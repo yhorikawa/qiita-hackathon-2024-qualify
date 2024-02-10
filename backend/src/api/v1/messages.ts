072041f97f7f6d785db6b340afcee9681358856c
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-02-10 07:53:42 +0000

    Update dependencies and add messages API

---
+++ backend/src/api/v1/messages.ts
@@ -0,0 +1,31 @@
+import { Hono } from "hono";
+import { jwt } from "hono/jwt";
+import { Bindings } from "./index";
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("*", async (c, next) => {
+  const setJwt = await jwt({ secret: c.env.JWT_SECRET, cookie: "accessToken" });
+  return setJwt(c, next);
+});
+
+const routes = app.post("/",
+zValidator(
+  'json',
+  z.object({
+    content: z.string(),
+  })
+)
+, async (c) => {
+  const payload = c.get("jwtPayload");
+  const { content } = await c.req.valid('json')
+  //const ok = createMessages({ content })
+  c.status(201);
+  return c.json({ success: true });
+});
+
+
+
+export default routes;
