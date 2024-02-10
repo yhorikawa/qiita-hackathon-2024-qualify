683ff2b950c79628f25bf5e1a85bf80a1fadee63
junseinagao <junsei.nagao@gmail.com>
2024-02-10 14:33:52 +0000

    🚧 useSentMessageList を実装

--- frontend/src/app/messages/me/page.tsx
+++ frontend/src/app/messages/me/page.tsx
@@ -1,20 +1,22 @@
+"use client";
+
 import Link from "next/link";
 import { NavigationLayout } from "#/components/NavigationLayout";
+import { useSentMessageList } from "./use-sent-message-list";
 
 export default function MessagesMeListPage() {
+  const { data, isLoading } = useSentMessageList();
+  if (!isLoading || !data) return null;
   return (
     <NavigationLayout>
       <ul className="flex flex-col">
-        {[...Array(10)].fill("誰かから来たメッセージ").map((message, i) => (
+        {data.messages.map((message, i) => (
           <li
-            key={`list-item-${
-              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-              i
-            }`}
+            key={message.id}
             className="inline-flex w-full items-center gap-x-2 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-slate-900 dark:border-gray-700 dark:text-white"
           >
             <Link href="#" className="w-full inline-block py-3 px-4">
-              {message}
+              {message.content}
             </Link>
           </li>
         ))}
---
+++ frontend/src/app/messages/me/use-sent-message-list.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+import { client } from "#/lib/client";
+
+const fetcher = async (_: string) => {
+  const res = await client.api.v1.messages.sent.$get();
+  if (!res.ok) throw new Error(String(res.status));
+  return await res.json();
+};
+
+export const useSentMessageList = () => {
+  const swr = useSWR("/api/v1/messages/sent", fetcher);
+  useEffect(() => {
+    if (swr.error) throw swr.error;
+  }, [swr.error]);
+  return swr;
+};
