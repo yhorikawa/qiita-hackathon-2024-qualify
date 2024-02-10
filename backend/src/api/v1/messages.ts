75f8c11453e6c6bf07cca860b4417a3ecab56e82
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-02-10 08:19:01 +0000

    Refactor message routes and add validation

--- backend/src/api/v1/messages.ts
+++ backend/src/api/v1/messages.ts
@@ -12,27 +12,55 @@ app.use("*", async (c, next) => {
   return setJwt(c, next);
 });
 
-const routes = app.post(
-  "/",
-  zValidator(
-    "json",
-    z.object({
-      content: z.string(),
-    }),
-  ),
-  async (c) => {
-    const payload = c.get("jwtPayload");
-    const { content } = await c.req.valid("json");
-    const createMessageParams = {
-      userId: payload.id,
-      category: "love",
-      content,
-    };
+const routes = app
+  .post(
+    "/",
+    zValidator(
+      "json",
+      z.object({
+        content: z.string(),
+      }),
+    ),
+    async (c) => {
+      const payload = c.get("jwtPayload");
+      const { content } = await c.req.valid("json");
+      const createMessageParams = {
+        userId: payload.id,
+        category: "love",
+        content,
+      };
 
-    await db.createMessage(c.env.DB, createMessageParams);
-    c.status(201);
-    return c.json({ success: true });
-  },
-);
+      await db.createMessage(c.env.DB, createMessageParams);
+      c.status(201);
+      return c.json({ success: true });
+    },
+  )
+  .post(
+    "/messages/:message_id/reply",
+    zValidator(
+      "json",
+      z.object({
+        content: z.string(),
+      }),
+    ),
+    async (c) => {
+      const payload = c.get("jwtPayload");
+      const { content } = await c.req.valid("json");
+      //const ok = createReplies({ content })
+      c.status(201);
+      return c.json({ success: true });
+    },
+  )
+  .get("/", async (c) => {
+    const payload = c.get("jwtPayload");
+    //const messages = getMessages()
+    return c.json({ success: true, messages: ["hello", "world"] });
+  })
+  .get("/:message_id", async (c) => {
+    const payload = c.get("jwtPayload");
+    const { message_id } = c.req.param();
+    //const message = getMessage({ message_id })
+    return c.json({ success: true, message: "hello" });
+  });
 
 export default routes;
