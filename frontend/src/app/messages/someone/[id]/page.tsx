7083d0c082f6e0f1a81ff48302ae8c60f2ec74dc
junseinagao <junsei.nagao@gmail.com>
2024-02-10 10:56:41 +0000

    💄 someoneパスのUI完成

---
+++ frontend/src/app/messages/someone/[id]/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Link from "next/link";
+import { useId, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { twMerge } from "tailwind-merge";
+import { NavsTabLayout } from "#/app/_ui-components/NavsTabLayout";
+import { MessageListNav } from "../../_dependencies/MessageListNav";
+
+type PageProps = {
+  params: { id: string };
+};
+
+export default function MessagesSomeoneDetailPage({
+  params: { id },
+}: PageProps) {
+  const textareaId = useId();
+  const [text, setText] = useState<string>("");
+  return (
+    <NavsTabLayout Navigation={<MessageListNav someoneMessageCount={0} />}>
+      <Link
+        href="#"
+        className="inline-flex items-center gap-x-2.5 text-sm text-blue-600 decoration-2 font-semibold dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+      >
+        {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+        <svg
+          className="flex-shrink-0 w-4 h-4"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="m15 18-6-6 6-6" />
+        </svg>
+        すべての送信メッセージ
+      </Link>
+      <div className="flex flex-col bg-white border border-gray-200 shadow-sm rounded-xl py-5 px-4 md:p-5 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 mt-4 leading-6 text-base font-normal">
+        あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
+        またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い巨きな石の建物のなかで考えていると、みんなむかし風のなつかしい青い幻燈のように思われます。
+        では、わたくしはいつかの小さなみだしをつけながら、しずかにあの年のイーハトーヴォの五月から十月までを書きつけましょう。
+      </div>
+      <div className={twMerge("pt-0.5", "mt-4")}>
+        <label
+          htmlFor={textareaId}
+          className="block text-sm mb-2 dark:text-white font-bold"
+        >
+          このメッセージに返信する
+        </label>
+        <TextareaAutosize
+          id={textareaId}
+          value={text}
+          onInput={(e) => setText(e.currentTarget.value)}
+          className={twMerge(
+            "py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600",
+          )}
+          minRows={10}
+          cacheMeasurements={true}
+          placeholder="送り主に伝えたいことを書こう"
+        />
+      </div>
+      <button
+        type="button"
+        className={twMerge(
+          "p-4 sm:p-5 flex items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
+          "mt-4 ml-auto",
+        )}
+      >
+        メッセージを送る
+      </button>
+    </NavsTabLayout>
+  );
+}
---
+++ frontend/src/app/messages/someone/[id]/sent/page.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from "tailwind-merge";
+import { NavsTabLayout } from "#/app/_ui-components/NavsTabLayout";
+import { MessageListNav } from "#/app/messages/_dependencies/MessageListNav";
+
+export default function MessagesSentPage() {
+  return (
+    <NavsTabLayout Navigation={<MessageListNav someoneMessageCount={0} />}>
+      <span className="w-[20rem] h-[20rem] flex justify-center items-center text-gray-500 font-bold bg-gray-200 dark:bg-gray-700 mx-auto">
+        Placeholder
+      </span>
+
+      <button
+        type="button"
+        className={twMerge(
+          "p-4 sm:p-5 flex w-full items-center justify-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
+          "mt-10",
+        )}
+      >
+        届いたメッセージの一覧にもどる
+      </button>
+    </NavsTabLayout>
+  );
+}
