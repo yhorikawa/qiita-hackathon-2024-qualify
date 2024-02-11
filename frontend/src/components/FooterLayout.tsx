e7bd0efe81d5638e6c87638ab5add301c2fb64f7
hikari0709 <tanimoto0709@gmail.com>
2024-02-11 03:44:33 +0000

    フッターレイアウトの変更: テキストスタイルと著作権表示の修正

--- frontend/src/components/FooterLayout.tsx
+++ frontend/src/components/FooterLayout.tsx
@@ -1,7 +1,7 @@
 export const FooterLayout = () => (
   <div className="bg-slate-700 text-center text-white p-3">
-    <p>Bottle Links</p>
-    <small className="mt-4">
+    <p className="font-semibold text-sm leading-5">Bottle Links</p>
+    <small className="mt-4 leading-4 font-normal text-xs">
       Copyright &copy; 5人のさむらい All Rights Reserved.
     </small>
   </div>
